5676c1cda46ac65d9ba17e58c28f1fa3c8ff4747
CUBINHO\CSHARPV5 <alberto.nunes@phoenix-santos.com.br>
2023-04-05 16:40:21 -0300

    primeiro estagio de desenvolvimento da api

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -22,25 +22,6 @@ namespace ProEventos.API.Controllers
             _context = context;
             
         }
-        public IEnumerable<Evento> _evento = new Evento[]{
-        new Evento(){
-                    EventoId = 1,
-                    Tema = "",
-                    Local = "",
-                    Lote = "1 lote",
-                    QtdPessoas = 250,
-                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-                    ImagemURL = "meucu.jpg"
-            },   new Evento(){
-                    EventoId = 2,
-                    Tema = "React11 e .NET 3",
-                    Local = "São Paulo",
-                    Lote = "2 lote",
-                    QtdPessoas = 250,
-                    DataEvento = DateTime.Now.AddDays(4).ToString("dd/MM/yyyy"),
-                    ImagemURL = "cu.jpg"
-            }
-        };
 
         [HttpGet]
         public IEnumerable<Evento> Get()
